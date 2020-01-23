f49def779d06519f0c10d73874c3ffd5118e8fa8
jonnelafin <elias.eelinen@gmail.com>
2020-01-23 15:47:10 +0200

    Added functionality to deep-clone BufferedImages

---
+++ src/JFUtils/Clone/Images.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package JFUtils.Clone;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+
+/**
+ *
+ * @author Jonnelafin
+ */
+public class Images {
+    static BufferedImage deepCopy(BufferedImage bi) {
+        ColorModel cm = bi.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = bi.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+}
